9a00aece414f7806d49af1f64f7e59defacdcf14
Fortunato666 <jorge_aquino_jr@hotmail.com>
2020-11-30 11:18:19 -0600

    Prueba Astrid

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a = 8;
+        b = 7;
+        c = 6;
+        d=a+b+c/3;
+        System.out.println(d);
+    }
+    
+}
